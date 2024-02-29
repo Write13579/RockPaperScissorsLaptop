d466a50c4b041156515799fecdb6282378b610e6
Patryk Baraniak <pb311239@student.polsl.pl>
2024-02-29 12:09:58 +0100

    dziala

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,103 @@
-let userChoice:string;
+let userChoice: string;
 const choices = ["rock", "paper", "scissors"];
-const scoreUserShow = document.getElementById("scoreUser") as HTMLHeadingElement;
-const scoreCompShow = document.getElementById("scoreComp") as HTMLHeadingElement;
+const scoreUserShow = document.getElementById(
+  "scoreUser"
+) as HTMLHeadingElement;
+const scoreCompShow = document.getElementById(
+  "scoreComp"
+) as HTMLHeadingElement;
 
-const showUserChoice = document.getElementById("userChoice") as HTMLHeadingElement;
-const showCompChoice = document.getElementById("compChoice") as HTMLHeadingElement;
+const showUserChoice = document.getElementById(
+  "userChoice"
+) as HTMLHeadingElement;
+const showCompChoice = document.getElementById(
+  "compChoice"
+) as HTMLHeadingElement;
+
+const result = document.getElementById("result") as HTMLDivElement;
 
 let user = 0;
 let comp = 0;
 
-function chosenRock(){
+function chosenRock() {
   userChoice = "rock";
   showUserChoice.textContent = `Your choice was ${userChoice}`;
-  if (losuj() === "rock"){
-    userWin();
-  }else{
+  let currentGuess = losuj();
+  if (currentGuess === "rock") {
+    draw();
+  } else if (currentGuess === "paper") {
     compWin();
+  } else if (currentGuess === "scissors") {
+    userWin();
+  } else {
+    console.log("error", losuj(), userChoice);
   }
 }
 
-function chosenPaper(){
+function chosenPaper() {
   userChoice = "paper";
   showUserChoice.textContent = `Your choice was ${userChoice}`;
-  if (losuj() === "paper"){
-    userWin();
-  }else{
-    compWin();
-  }}
+  let currentGuess = losuj();
 
-function chosenScissors(){
+  if (currentGuess === "paper") {
+    draw();
+  } else if (currentGuess === "scissors") {
+    compWin();
+  } else if (currentGuess === "rock") {
+    userWin();
+  } else {
+    console.log("error");
+  }
+}
+
+function chosenScissors() {
   userChoice = "scissors";
   showUserChoice.textContent = `Your choice was ${userChoice}`;
-  if (losuj() === "scissors"){
-    userWin();
-  }else{
-    compWin();
-  }}
+  let currentGuess = losuj();
 
-function losuj(){
+  if (currentGuess === "scissors") {
+    draw();
+  } else if (currentGuess === "rock") {
+    compWin();
+  } else if (currentGuess === "paper") {
+    userWin();
+  } else {
+    console.log("error");
+  }
+}
+
+function losuj() {
   let idxLos = Math.floor(Math.random() * 3);
   showCompChoice.textContent = `Computer choice was ${choices[idxLos]}`;
-  return choices[idxLos];  
+  return choices[idxLos];
 }
 
-function userWin(){
+function draw() {
   user++;
+  comp++;
   scoreUserShow.textContent = user.toString();
+  scoreCompShow.textContent = comp.toString();
+  result.textContent = "its a draw";
 }
 
-function compWin(){
+function compWin() {
   comp++;
   scoreCompShow.textContent = comp.toString();
+  result.textContent = "computer wins";
 }
 
-function resetScores(){
+function userWin() {
+  user++;
+  scoreUserShow.textContent = user.toString();
+  result.textContent = "you win";
+}
+
+function resetScores() {
   user = 0;
   comp = 0;
   scoreUserShow.textContent = "0";
   scoreCompShow.textContent = "0";
+  showUserChoice.textContent = "";
+  showCompChoice.textContent = "";
+  result.textContent = "";
 }
-
-//zamienic userwin na draw xddd i wgl to gowno naprawic
